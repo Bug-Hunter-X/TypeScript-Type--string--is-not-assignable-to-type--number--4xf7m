231ae4f474364dbeb6d07143c339a9a2ec4db4c9
BugHunter-X <askzenai@gmail.com>
2025-01-05 18:22:05 +0100

    Add bug.ts file

---
+++ bug.ts
@@ -0,0 +1,13 @@
+function add(a: number, b: number): number {
+  return a + b;
+}
+
+function subtract(a: number, b: number): number {
+  return a - b;
+}
+
+const result1 = add(5, 3); // 8
+const result2 = subtract(10, 4); // 6
+
+//Uncommon error: Type 'string' is not assignable to type 'number'.
+const result3 = add(5, "3");
